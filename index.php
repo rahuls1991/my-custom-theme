535136c104a594956154589a5a27358b247faf86
Rahul <rahuls@zealousweb.com>
2025-08-25 19:41:31 +0530

    Update index file

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main id="site-content">
-    <h1>Hello, this is My Custom Theme!!!!</h1>
+    <h1>Hello, this is My Custom Theme!!!!..</h1>
 </main>
 
 <?php get_footer(); ?>
